8b0a7c4b5ea28370e086c7974fee5ea3598bbc7a
Wataru.Kasahara <kasahara1019@gmail.com>
2020-05-15 23:22:50 +0900

    feat: Launch server

--- app.ts
+++ app.ts
@@ -1,1 +1,7 @@
-console.log("Hello deno");
+import { serve } from "https://deno.land/std/http/server.ts";
+
+const s = serve({ port: 8000 });
+
+for await (const req of s) {
+  req.respond({ body: "Hello World\n" });
+}
